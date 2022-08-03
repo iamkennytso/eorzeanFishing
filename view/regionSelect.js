15b660464617f0e1be4b5e365f23daae13bd437e
iamkennytso <iamkennytso@gmail.com>
2022-08-03 14:40:55 -0400

    navigation

---
+++ view/regionSelect.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function RegionSelect() {
+  return <View style={styles.meh}>
+    <Text>helloWorld</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({
+  meh: {
+    flex: 1,
+    backgroundColor: '#0000FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
