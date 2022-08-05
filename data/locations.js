b347fbf2942c86356f945c2f6fe581900e78c230
Kenneth Tso <iamkennytso@gmail.com>
2022-08-05 19:58:06 -0400

    Update locations.js

--- data/locations.js
+++ data/locations.js
@@ -1,489 +1,411 @@
 export const regions = [
-	"La Noscea",
-	"The Black Shroud",
-	"Thanalan",
-	"Coerthas",
-	"Mor Dhona"
+  "La Noscea",
+  "The Black Shroud",
+  "Thanalan",
+  "Coerthas",
+  "Mor Dhona"
 ];
 
 export const areas = {
   "La Noscea": {
     "Limsa Lominsa Upper Decks": [
       {
-        "n": "Limsa Lominsa Upper Decks",
-        "l": 1,
-        "t": 1,
-        "r": 0
+        n: "Limsa Lominsa Upper Decks", l: 1, t: 1,
       },
     ],
     "Limsa Lominsa Lower Decks": [
       {
-        "n": "Limsa Lominsa Lower Decks",
-        "l": 1,
-        "t": 1,
-        "r": 0
+        n: "Limsa Lominsa Lower Decks", l: 1, t: 1,
       },
     ],
     "Middle La Noscea": [
       {
-        "n": "Rogue River",
-        "l": 1,
-        "t": 1,
-        "r": 0
+        n: "Rogue River", l: 1, t: 1,
       },
       {
-        "n": "West Agelyss River",
-        "l": 1,
-        "t": 0,
-        "r": 0
+        n: "West Agelyss River", l: 1, t: 0,
       },
       {
-        "n": "Zephyr Drift",
-        "l": 1,
-        "t": 1,
-        "r": 0
+        n: "Zephyr Drift", l: 1, t: 1,
       },
       {
-        "n": "Nym River",
-        "l": 5,
-        "t": 0,
-        "r": 0
+        n: "Nym River", l: 5, t: 0,
       },
       {
-        "n": "Summerford",
-        "l": 5,
-        "t": 1,
-        "r": 0
+        n: "Summerford", l: 5, t: 1,
       },
       {
-        "n": "Woad Whisper Canyon",
-        "l": 15,
-        "t": 0,
-        "r": 1
+        n: "Woad Whisper Canyon", l: 15, t: 0,
       },
     ],
     "Lower La Noscea": [
       {
-        "n": "The Mourning Widow",
-        "l": 1,
-        "t": 0,
-        "r": 0
+        n: "The Mourning Widow", l: 1, t: 0,
       },
       {
-        "n": "Moraby Bay",
-        "l": 5,
-        "t": 1,
-        "r": 0
+        n: "Moraby Bay", l: 5, t: 1,
       },
       {
-        "n": "Candlekeep Quay",
-        "l": 10,
-        "t": 1,
-        "r": 0
+        n: "Candlekeep Quay", l: 10, t: 1,
       },
       {
-        "n": "Cedarwood",
-        "l": 10,
-        "t": 1,
-        "r": 0
+        n: "Cedarwood", l: 10, t: 1,
       },
       {
-        "n": "Oschon's Torch",
-        "l": 10,
-        "t": 1,
-        "r": 0
+        n: "Oschon's Torch", l: 10, t: 1,
       },
       {
-        "n": "Empty Heart",
-        "l": 15,
-        "t": 0,
-        "r": 0
+        n: "Empty Heart", l: 15, t: 0,
       },
       {
-        "n": "Moraby Drydocks",
-        "l": 15,
-        "t": 1,
-        "r": 0
+        n: "Moraby Drydocks", l: 15, t: 1,
       },
       {
-        "n": "Blind Iron Mines",
-        "l": 20,
-        "t": 0,
-        "r": 0
+        n: "Blind Iron Mines", l: 20, t: 0,
       },
       {
-        "n": "The Salt Strand",
-        "l": 20,
-        "t": 1,
-        "r": 2
+        n: "The Salt Strand", l: 20, t: 1,
       },
     ],
     "Eastern La Noscea": [
       {
-        "n": "South Bloodshore",
-        "l": 25,
-        "t": 1,
-        "r": 0
+        n: "South Bloodshore", l: 25, t: 1,
       },
       {
-        "n": "Costa del Sol",
-        "l": 30,
-        "t": 1,
-        "r": 0
+        n: "Costa del Sol", l: 30, t: 1,
       },
       {
-        "n": "Hidden Falls",
-        "l": 35,
-        "t": 0,
-        "r": 2
+        n: "Hidden Falls", l: 35, t: 0,
       },
       {
-        "n": "North Bloodshore",
-        "l": 35,
-        "t": 1,
-        "r": 0
+        n: "North Bloodshore", l: 35, t: 1,
       },
       {
-        "n": "East Agelyss River",
-        "l": 40,
-        "t": 0,
-        "r": 0
+        n: "East Agelyss River", l: 40, t: 0,
       },
       {
-        "n": "Raincatcher Gully",
-        "l": 40,
-        "t": 0,
-        "r": 0
+        n: "Raincatcher Gully", l: 40, t: 0,
       },
       {
-        "n": "The Juggernaut",
-        "l": 40,
-        "t": 0,
-        "r": 0
+        n: "The Juggernaut", l: 40, t: 0,
       },
       {
-        "n": "Red Mantis Falls",
-        "l": 50,
-        "t": 0,
-        "r": 3
+        n: "Red Mantis Falls", l: 50, t: 0,
       },
       {
-        "n": "North Isle of Endless Summer",
-        "l": 50,
-        "t": 1,
-        "r": 0
+        n: "North Isle of Endless Summer", l: 50, t: 1,
       },
       {
-        "n": "Rhotano Sea (Privateer Forecastle)",
-        "l": 50,
-        "t": 1,
-        "r": 0
+        n: "Rhotano Sea (Privateer Forecastle)", l: 50, t: 1,
       },
       {
-        "n": "Rhotano Sea (Privateer Sterncastle)",
-        "l": 1,
-        "t": 1,
-        "r": 3
+        n: "Rhotano Sea (Privateer Sterncastle)", l: 1, t: 1,
       },
     ],
     "Western La Noscea": [
       {
-        "n": "Swiftperch",
-        "l": 10,
-        "t": 1,
-        "r": 0
+        n: "Swiftperch", l: 10, t: 1,
       },
       {
-        "n": "Skull Valley",
-        "l": 15,
-        "t": 1,
-        "r": 0
+        n: "Skull Valley", l: 15, t: 1,
       },
       {
-        "n": "The Brewer's Beacon",
-        "l": 15,
-        "t": 1,
-        "r": 1
+        n: "The Brewer's Beacon", l: 15, t: 1,
       },
       {
-        "n": "Halfstone",
-        "l": 35,
-        "t": 1,
-        "r": 0
+        n: "Halfstone", l: 35, t: 1,
       },
       {
-        "n": "Isles of Umbra Northshore",
-        "l": 40,
-        "t": 1,
-        "r": 0
+        n: "Isles of Umbra Northshore", l: 40, t: 1,
       },
       {
-        "n": "Sapsa Spawning Grounds",
-        "l": 45,
-        "t": 1,
-        "r": 0
+        n: "Sapsa Spawning Grounds", l: 45, t: 1,
       },
       {
-        "n": "The Ship Graveyard",
-        "l": 45,
-        "t": 1,
-        "r": 0
+        n: "The Ship Graveyard", l: 45, t: 1,
       },
       {
-        "n": "Isles of Umbra Southshore",
-        "l": 50,
-        "t": 1,
-        "r": 3
+        n: "Isles of Umbra Southshore", l: 50, t: 1,
       },
       {
-        "n": "Sapsa Spawning Grounds - Reaver Hide",
-        "l": 50,
-        "t": 1,
-        "r": 0
+        n: "Sapsa Spawning Grounds - Reaver Hide", l: 50, t: 1,
       },
     ],
     "Upper La Noscea": [
       {
-        "n": "Fool Falls",
-        "l": 20,
-        "t": 0,
-        "r": 0
+        n: "Fool Falls", l: 20, t: 0,
       },
       {
-        "n": "Oakwood",
-        "l": 20,
-        "t": 0,
-        "r": 0
+        n: "Oakwood", l: 20, t: 0,
       },
       {
-        "n": "Bronze Lake Shallows",
-        "l": 30,
-        "t": 0,
-        "r": 0
+        n: "Bronze Lake Shallows", l: 30, t: 0,
       },
       {
-        "n": "Northeast Bronze Lake",
-        "l": 35,
-        "t": 0,
-        "r": 0
+        n: "Northeast Bronze Lake", l: 35, t: 0,
       },
     ],
     "Outer La Noscea": [
       {
-        "n": "Northwest Bronze Lake",
-        "l": 35,
-        "t": 0,
-        "r": 0
+        n: "Northwest Bronze Lake", l: 35, t: 0,
       },
       {
-        "n": "The Long Climb",
-        "l": 40,
-        "t": 0,
-        "r": 3
+        n: "The Long Climb", l: 40, t: 0,
       },
     ],
     Mist: [
       {
-        "n": "Mist",
-        "l": 30,
-        "t": 1,
-        "r": 0
+        n: "Mist", l: 30, t: 1,
       },
     ]
   },
   "The Black Shroud": {
     "New Gridania": [
       {
-        "n": "Jadeite Flood",
-        "l": 5,
-        "t": 0,
-        "r": 0
+        n: "Jadeite Flood", l: 5, t: 0,
       },
       {
-        "n": "Lower Black Tea Brook",
-        "l": 5,
-        "t": 0,
-        "r": 0
+        n: "Lower Black Tea Brook", l: 5, t: 0,
       },
     ],
     "Old Gridania": [
       {
-        "n": "Upper Black Tea Brook",
-        "l": 5,
-        "t": 0,
-        "r": 0
+        n: "Upper Black Tea Brook", l: 5, t: 0,
       },
       {
-        "n": "Whispering Gorge",
-        "l": 5,
-        "t": 0,
-        "r": 0
+        n: "Whispering Gorge", l: 5, t: 0,
       },
     ],
     "Central Shroud": [
       {
-        "n": "The Vein",
-        "l": 5,
-        "t": 0,
-        "r": 0
+        n: "The Vein", l: 5, t: 0,
       },
       {
-        "n": "The Mirror",
-        "l": 15,
-        "t": 0,
-        "r": 0
+        n: "The Mirror", l: 15, t: 0,
       },
       {
-        "n": "Everschade",
-        "l": 30,
-        "t": 0,
-        "r": 0
+        n: "Everschade", l: 30, t: 0,
       },
       {
-        "n": "Haukke Manor (Landmark)",
-        "l": 35,
-        "t": 0,
-        "r": 0
+        n: "Haukke Manor (Landmark)", l: 35, t: 0,
       },
       {
-        "n": "Hopeseed Pond",
-        "l": 35,
-        "t": 0,
-        "r": 0
+        n: "Hopeseed Pond", l: 35, t: 0,
       },
     ],
     "East Shroud": [
       {
-        "n": "Sweetbloom Pier",
-        "l": 15,
-        "t": 0,
-        "r": 0
+        n: "Sweetbloom Pier", l: 15, t: 0,
       },
       {
-        "n": "Springripple Brook",
-        "l": 15,
-        "t": 0,
-        "r": 0
+        n: "Springripple Brook", l: 15, t: 0,
       },
       {
-        "n": "Sanctum of the Twelve",
-        "l": 20,
-        "t": 0,
-        "r": 0
+        n: "Sanctum of the Twelve", l: 20, t: 0,
       },
       {
-        "n": "Verdant Drop",
-        "l": 25,
-        "t": 0,
-        "r": 0
+        n: "Verdant Drop", l: 25, t: 0,
       },
       {
-        "n": "Sylphlands",
-        "l": 45,
-        "t": 0,
-        "r": 0
+        n: "Sylphlands", l: 45, t: 0,
       },
     ],
     "South Shroud": [
       {
-        "n": "Upper Hathoeva River",
-        "l": 20,
-        "t": 0,
-        "r": 0
+        n: "Upper Hathoeva River", l: 20, t: 0,
       },
       {
-        "n": "East Hathoeva River",
-        "l": 25,
-        "t": 0,
-        "r": 0
+        n: "East Hathoeva River", l: 25, t: 0,
       },
       {
-        "n": "Lower Hathoeva River",
-        "l": 25,
-        "t": 0,
-        "r": 0
+        n: "Lower Hathoeva River", l: 25, t: 0,
       },
       {
-        "n": "Goblinblood",
-        "l": 30,
-        "t": 0,
-        "r": 0
+        n: "Goblinblood", l: 30, t: 0,
       },
       {
-        "n": "Middle Hathoeva River",
-        "l": 30,
-        "t": 0,
-        "r": 2
+        n: "Middle Hathoeva River", l: 30, t: 0,
       },
       {
-        "n": "Rootslake",
-        "l": 35,
-        "t": 0,
-        "r": 0
+        n: "Rootslake", l: 35, t: 0,
       },
       {
-        "n": "Urth's Gift",
-        "l": 45,
-        "t": 0,
-        "r": 0
+        n: "Urth's Gift", l: 45, t: 0,
       },
     ],
     "North Shroud": [
       {
-        "n": "Fallgourd Float",
-        "l": 15,
-        "t": 0,
-        "r": 0
+        n: "Fallgourd Float", l: 15, t: 0,
       },
       {
-        "n": "Murmur Rills",
-        "l": 15,
-        "t": 0,
-        "r": 0
+        n: "Murmur Rills", l: 15, t: 0,
       },
       {
-        "n": "Proud Creek",
-        "l": 40,
-        "t": 0,
-        "r": 0
+        n: "Proud Creek", l: 40, t: 0,
       },
       {
-        "n": "Lake Tahtotl",
-        "l": 50,
-        "t": 0,
-        "r": 3
+        n: "Lake Tahtotl", l: 50, t: 0,
       },
     ],
     "Lavender Beds": [
       {
-        "n": "Lavender Beds",
-        "l": 30,
-        "t": 0,
-        "r": 0
+        n: "Lavender Beds", l: 30, t: 0,
+      },
+    ]
+  },
+  Thanalan: {
+    "Western Thanalan": [
+      {
+        n: "Nophica's Wells", l: 5, t: 0,
+      },
+      {
+        n: "The Footfalls", l: 10, t: 0,
+      },
+      {
+        n: "Vesper Bay", l: 15, t: 1,
+      },
+      {
+        n: "Crescent Cove", l: 20, t: 1,
+      },
+      {
+        n: "The Silver Bazaar", l: 25, t: 1,
+      },
+      {
+        n: "Cape Westwind", l: 45, t: 1,
+      },
+      {
+        n: "Moondrip", l: 45, t: 0,
+      },
+      {
+        n: "Parata's Peace", l: 50, t: 0,
+      },
+    ],
+    "Central Thanalan": [
+      {
+        n: "Upper Soot Creek", l: 5, t: 0,
+      },
+      {
+        n: "Lower Soot Creek", l: 10, t: 0,
+      },
+      {
+        n: "The Clutch", l: 10, t: 0,
+      },
+      {
+        n: "The Unholy Heir", l: 15, t: 0,
+      },
+    ],
+    "Eastern Thanalan": [
+      {
+        n: "North Drybone", l: 15, t: 0,
+      },
+      {
+        n: "South Drybone", l: 15, t: 0,
+      },
+      {
+        n: "Yugr'am River", l: 30, t: 0,
+      },
+      {
+        n: "The Burning Wall", l: 30, t: 0,
+      },
+    ],
+    "Southern Thanalan": [
+      {
+        n: "Burnt Lizard Creek", l: 25, t: 0,
+      },
+      {
+        n: "Forgotten Springs", l: 35, t: 2,
+      },
+      {
+        n: "Sagolii Desert", l: 35, t: 2,
+      },
+      {
+        n: "Sagolii Dunes", l: 35, t: 0,
+      },
+      {
+        n: "Zahar'ak", l: 35, t: 0,
+      },
+    ],
+    "Northern Thanalan": [
+      {
+        n: "Bluefog", l: 40, t: 0,
+      },
+      {
+        n: "Ceruleum Field", l: 45, t: 0,
+      },
+    ],
+    "The Goblet": [
+      {
+        n: "The Goblet", l: 30, t: 0,
+      },
+    ]
+  },
+  Coerthas: {
+    "Coerthas Central Highlands": [
+      {
+        n: "Coerthas River", l: 35, t: 0,
+      },
+      {
+        n: "Daniffen Pass", l: 35, t: 0,
+      },
+      {
+        n: "Dragonhead Latrines", l: 40, t: 0,
+      },
+      {
+        n: "Exploratory Ice Hole", l: 40, t: 0,
+      },
+      {
+        n: "The Nail", l: 40, t: 0,
+      },
+      {
+        n: "Sea of Clouds", l: 45, t: 3,
+      },
+      {
+        n: "Snowcloak (Landmark)", l: 45, t: 0
+      },
+      {
+        n: "The Weeping Saint", l: 45, t: 0
+      },
+      {
+        n: "Witchdrop", l: 50, t: 0
+      },
+    ],
+  },
+  "Mor Dhona": {
+    "Mor Dhona": [
+      {
+        n: "North Silvertear", l: 45, t: 0
+      },
+      {
+        n: "Singing Shards", l: 45, t: 0
+      },
+      {
+        n: "The Deep Tangle", l: 45, t: 0
+      },
+      {
+        n: "Rathefrost", l: 50, t: 0
+      },
+      {
+        n: "The North Shards", l: 50, t: 0
+      },
+      {
+        n: "The Tangle", l: 50, t: 0
       },
     ]
   }
 }
 
-const meh = [
-  {
-    "n": "LALALA",
-    "l": 1,
-    "t": 0,
-    "r": 0
-  },
-]
-
 export const poolTypes = [
   'Freshwater',
-  'Saltwater'
+  'Saltwater',
+  'Dunefishing',
+  'Cloudfishing'
 ]
 
-// export const pools = {
-//   "Limsa Lominsa Upper Decks": {
-//     "img": "lalludllud",
-//     "fish": [
-				
-//     ]
-//   }
-// }
+export const requirements = [
+  null,
+  'Gulleye I',
+  'Gulleye II',
+  'Gulleye III',
+  'Dunefishing',
+  'Cloudfishing'
+]
