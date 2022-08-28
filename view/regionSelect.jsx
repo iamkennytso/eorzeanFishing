d40d1d8f7d411fc2d367ec3852c79df6b8cfe461
iamkennytso <iamkennytso@gmail.com>
2022-08-27 21:31:19 -0400

    deployment attempt

--- view/regionSelect.jsx
+++ view/regionSelect.jsx
@@ -19,7 +19,7 @@ export default function RegionSelect({ navigation }) {
           <Text style={subtitleStyles}>{region}</Text>
         </TouchableGradient>
       </View>)}
-      <TouchableGradient
+      {/* <TouchableGradient
         onPress={() => navigation.navigate(FISH_GUIDE)}
         customGradientStyles={styles.regionGradient}
       >
@@ -27,7 +27,7 @@ export default function RegionSelect({ navigation }) {
           <Image style={{height: 32, width: 32}} source={require('../assets/misc/fisher.png')} />
           <Text style={subtitleStyles}>  Fish Guide</Text>
         </View>
-      </TouchableGradient>
+      </TouchableGradient> */}
     </ScrollView>
   </>
 }
